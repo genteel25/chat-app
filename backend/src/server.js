fab59b694de080d4395ec5f3b5c14e145ef94d76
Ajagbe quadri <qajagbe@africaexchange.com>
2025-09-14 15:32:55 +0100

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,22 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
+import path from 'path'
 
 const app = express();
+const __dirname = path.resolve();
 dotenv.config() 
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+    })
+}
 
 
 
