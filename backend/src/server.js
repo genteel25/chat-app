bf338fb8bdfc334d55918132df86f2f60d404fc9
Ajagbe quadri <qajagbe@africaexchange.com>
2025-09-22 15:22:23 +0100

    implemented update profile endpoint and auth check endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import path from 'path'
@@ -11,6 +12,7 @@ const __dirname = path.resolve();
 dotenv.config() 
 
 app.use(express.json()) // req.body
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
