870f35b400d54ef826f9c868c3e47de3c267a245
Ajagbe quadri <qajagbe@africaexchange.com>
2025-11-06 21:11:51 +0100

    messages impl feature

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,84 @@
+// import Message from "../models/Message.js"
+import cloudinary from "../lib/cloudinary.js"
+import User from "../models/User.js"
+import Message from "../models/message.js";
+
+
+export const getAllContacts = async (req, res, _) => {
+    try {
+        const loggedInUserId = req.user._id
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
+        res.status(200).json(filteredUsers)
+    } catch (e) { }
+}
+
+export const getMessagesByUserId = async (req, res, _) => {
+    try {
+        const myId = req.user._id; 
+        const {id: userToChatId} = req.params;
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId},
+            ]
+        }).sort({createdAt: 1})
+        res.status(200).json(messages)
+
+    } catch (e) { 
+        return res.status(500).json({error: e.message})
+    }
+}
+
+export const sendMessage = async (req, res, _ ) => {
+    try {
+        const senderId = req.user._id; 
+        const {id: receiverId} = req.params;
+        const {text, image} = req.body;
+
+        let imageUrl;
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url
+        }
+        const message = new Message({
+            senderId,
+            receiverId, 
+            text,
+            image: imageUrl,
+        })
+        await message.save()
+
+        // todo: send message in real time if usser is online - socket.io
+        res.status(201).json(message)
+    } catch (e) { 
+        console.log(e)
+        return res.status(500).json({error: "Internal server error"})
+    }
+}
+
+export const getChatPartners = async (req, res, _) => {
+    try {
+        const myId = req.user._id; 
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId},
+                {receiverId: myId},
+            ]
+        }).sort({createdAt: 1})
+
+        const chatPatnerIds = [
+            ...new Set( 
+                messages.map(msg => 
+                        msg.senderId.toString() === myId.toString()
+                    ? msg.receiverId.toString() 
+                    : msg.senderId.toString()
+                )
+            )
+        ]
+        const chatPartners = await User.find({_id: {$in: chatPatnerIds}}).select("-password")
+        res.status(200).json(chatPartners)
+
+    } catch (e) { 
+        return res.status(500).json({error: "Internal server error"})
+    }
+}
---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,34 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+import express from "express";
+import { ENV } from "../lib/env.js";
+
+const app = express();
+const port = 3000;
+
+const aj = arcjet({
+  key: ENV.ARCJET_KEY,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "DRY_RUN", // Blocks requests. Use "LIVE" to block them
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    slidingWindow({
+        mode: "LIVE",
+        max: 2000, 
+        interval: 60000, 
+    })
+  ],
+});
+
+export default aj;
