9ab4ee24ef28f0dc18d96044c17fc3636afee475
Ajagbe quadri <qajagbe@africaexchange.com>
2025-11-17 16:27:25 +0100

    signup implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import messageRoutes from './routes/message.route.js'
 import path from 'path'
 import connectDB from './lib/db.js'
 import { ENV } from './lib/env.js';
+import cors from 'cors'
 
 const app = express();
 const __dirname = path.resolve();
@@ -13,6 +14,7 @@ dotenv.config()
 
 
 app.use(express.json()) // req.body
+app.use(cors({origin: ENV.CLIENT_URL,credentials: true}))
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,37 @@
 import {create} from "zustand"
+import { axiosInstance } from "../lib/axios"
+// import { signup } from "../../../backend/src/controllers/auth.controller";
+import toast from 'react-hot-toast'
+
+
 
 export const useAuthStore = create((set) => ({
-    authUser: {name: "john", _id: 123, age: 25},
-    isLoading: false,
-    isLoggedIn: false,
-    login: () => {
-        console.log("We jus logged in")
-        set({isLoading: true})
+    authUser: null,
+    isCheckingAuth: true,
+    isSigningUp: false,
+    signup: async (formData) => {
+        try{
+            set({isSigningUp:true})
+            const res = await axiosInstance.post("/auth/signup", formData);
+            set({authUser: res.data})
+            //toast
+            toast.success("Account created successfully")
+        } catch(e) {
+            console.log("Error signing up", e);
+            toast.error(e.response.data.message || "Error signing up")
+        }finally{
+            set({isSigningUp:false})
+        }
+    },
+    checkAuth:async () => {
+       try{
+        const res = await axiosInstance.get("/auth/check");
+        set({authUser: res.data})
+       } catch(e) {
+        console.log("Error checking auth", e);
+        set({authUser: null})
+       }finally {
+        set({isCheckingAuth: false})
+       }
     }
 }))
