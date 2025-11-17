d673a7b12522e903a931a47824c4485b5618bcd1
Ajagbe quadri <qajagbe@africaexchange.com>
2025-11-17 17:09:01 +0100

    login-logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,10 +5,23 @@ import toast from 'react-hot-toast'
 
 
 
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set, get) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
+    
+    checkAuth:async () => {
+       try{
+        const res = await axiosInstance.get("/auth/check");
+        set({authUser: res.data})
+       } catch(e) {
+        console.log("Error checking auth", e);
+        set({authUser: null})
+       }finally {
+        set({isCheckingAuth: false})
+       }
+    },
     signup: async (formData) => {
         try{
             set({isSigningUp:true})
@@ -23,15 +36,62 @@ export const useAuthStore = create((set) => ({
             set({isSigningUp:false})
         }
     },
-    checkAuth:async () => {
-       try{
-        const res = await axiosInstance.get("/auth/check");
-        set({authUser: res.data})
-       } catch(e) {
-        console.log("Error checking auth", e);
-        set({authUser: null})
-       }finally {
-        set({isCheckingAuth: false})
-       }
+    login: async (formData) => {
+        try{
+            set({isLoggingIn:true})
+            const res = await axiosInstance.post("/auth/login", formData);
+            set({authUser: res.data})
+            //toast
+            toast.success("Login successful")
+        } catch(e) {
+            console.log("Error logging in", e);
+            toast.error(e.response.data.message || "Error logging in")
+        }finally{
+            set({isLoggingIn:false})
+        }
+    },
+    logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    //   get().disconnectSocket();
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
     }
+  },
+
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response.data.message);
+    }
+  },
+
+//   connectSocket: () => {
+//     const { authUser } = get();
+//     if (!authUser || get().socket?.connected) return;
+
+//     const socket = io(BASE_URL, {
+//       withCredentials: true, // this ensures cookies are sent with the connection
+//     });
+
+//     socket.connect();
+
+//     set({ socket });
+
+//     // listen for online users event
+//     socket.on("getOnlineUsers", (userIds) => {
+//       set({ onlineUsers: userIds });
+//     });
+//   },
+
+//   disconnectSocket: () => {
+//     if (get().socket?.connected) get().socket.disconnect();
+//   },
 }))
